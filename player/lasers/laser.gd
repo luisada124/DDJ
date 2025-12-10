c7a59378f5929c572285eb48827b25810baad1a5
luisada124 <luis.miguel.colaco@gmail.com>
2025-12-10 03:18:51 +0000

    laser

--- player/lasers/laser.gd
+++ player/lasers/laser.gd
@@ -1,13 +1,18 @@
 extends Area2D
 
-@export var speed := 800.0
-var direction: Vector2 = Vector2.UP   # vai ser definido pela nave
-var lifetime := 2.0                      # segundos até desaparecer
-
+@export var speed: float = 800.0
+@export var damage: int = 5
+var direction: Vector2 = Vector2.UP
+var lifetime: float = 2.0
 
 func _physics_process(delta: float) -> void:
-	global_position += direction * speed * delta	
+	global_position += direction * speed * delta
 
 	lifetime -= delta
 	if lifetime <= 0.0:
 		queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("comet") and body.has_method("take_damage"):
+		body.take_damage(damage)
+	queue_free()
