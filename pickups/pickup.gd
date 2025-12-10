10dbd80263a27062c4e3dec793c973c95bdefbcc
luisada124 <luis.miguel.colaco@gmail.com>
2025-12-10 02:02:00 +0000

    pickup inicial

---
+++ pickups/pickup.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@export var resource_type: String = "scrap"
+@export var amount: int = 1
+@export var spin_speed: float = 2.0
+
+func _ready() -> void:
+	add_to_group("pickup")
+
+func _process(delta: float) -> void:
+	rotation += spin_speed * delta	# só para dar um efeito a girar
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		GameState.add_resource(resource_type, amount)
+		queue_free()
